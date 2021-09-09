65d481ba8bcf2f0add98deceaac4b2e847efbaf9
EdvardXYZ <eddumansky@gmail.com>
2021-09-09 20:44:02 +0300

    Create AggregateFilePaths function

--- TheEyeTether.UnitTests/Tests/FilePathAggregatorTests.cs
+++ TheEyeTether.UnitTests/Tests/FilePathAggregatorTests.cs
@@ -1,6 +1,17 @@
+using System.Collections.Generic;
+using TheEyeTether.Types;
+using Xunit;
+
 namespace TheEyeTether.UnitTests
 {
     public class FilePathAggregatorTests
     {
+        [Fact]
+        public void AggregateFilePaths_ReturnsListOfStrings_WhenCalled()
+        {
+            var results = FilePathAggregator.AggregateFilePaths();
+
+            Assert.IsType<List<string>>(results);
+        }
     }
 }
--- TheEyeTether/Types/FilePathAggregator.cs
+++ TheEyeTether/Types/FilePathAggregator.cs
@@ -1,6 +1,12 @@
+using System.Collections.Generic;
+
 namespace TheEyeTether.Types
 {
     public static class FilePathAggregator
     {
+        public static List<string> AggregateFilePaths()
+        {
+            return new List<string>();
+        }
     }
 }
