43e8d0adcbbdbd4ac22b1b3384cd053124a3661e
EdvardXYZ <eddumansky@gmail.com>
2021-09-09 21:23:37 +0300

    Return paths of files with matching file name

--- TheEyeTether.UnitTests/Tests/FilePathAggregatorTests.cs
+++ TheEyeTether.UnitTests/Tests/FilePathAggregatorTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
 using TheEyeTether.Types;
 using Xunit;
 
@@ -7,11 +8,31 @@ namespace TheEyeTether.UnitTests
     public class FilePathAggregatorTests
     {
         [Fact]
-        public void AggregateFilePaths_ReturnsListOfStrings_WhenCalled()
+        public void AggregateFilePaths_ReturnsArrayOfStrings_WhenCalled()
         {
-            var results = FilePathAggregator.AggregateFilePaths();
+            var fileName = string.Empty;
+            var searchDirectoryPath = string.Empty;
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>());
 
-            Assert.IsType<List<string>>(results);
+            var results = FilePathAggregator.AggregateFilePaths(fileName, searchDirectoryPath, fileSystem);
+
+            Assert.IsType<string[]>(results);
+        }
+
+        [Fact]
+        public void AggregateFilePaths_ReturnsFilePathsWithMatchingFileName_WhenFilesExist()
+        {
+            var fileName = "test.txt";
+            var searchDirectoryPath = @"C:\Test\SearchDirectory\";
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { searchDirectoryPath + @"SubDirectory1\" + fileName, new MockFileData(string.Empty) },
+                { searchDirectoryPath + @"SubDirectory2\" + fileName, new MockFileData(string.Empty) },
+            });
+
+            var results = FilePathAggregator.AggregateFilePaths(fileName, searchDirectoryPath, fileSystem);
+
+            Assert.Equal(2, results.Length);
         }
     }
 }
--- TheEyeTether/Types/FilePathAggregator.cs
+++ TheEyeTether/Types/FilePathAggregator.cs
@@ -1,12 +1,17 @@
-using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
 
 namespace TheEyeTether.Types
 {
     public static class FilePathAggregator
     {
-        public static List<string> AggregateFilePaths()
+        public static string[] AggregateFilePaths(
+                string fileName,
+                string searchDirectoryPath,
+                IFileSystem fileSystem)
         {
-            return new List<string>();
+            return fileSystem.Directory.GetFiles(searchDirectoryPath, fileName,
+                    SearchOption.AllDirectories);
         }
     }
 }
