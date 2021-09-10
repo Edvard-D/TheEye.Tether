c775d47b3235e780309bc2097c557c5fe358391a
EdvardXYZ <eddumansky@gmail.com>
2021-09-10 11:30:38 +0300

    Throw exception when called on Linux

--- TheEyeTether.UnitTests/Tests/OSVersionHelpersTests.cs
+++ TheEyeTether.UnitTests/Tests/OSVersionHelpersTests.cs
@@ -1,6 +1,25 @@
+using System.Runtime.InteropServices;
+using TheEyeTether.Helpers;
+using TheEyeTether.UnitTests.Stubs;
+using Xunit;
+
 namespace TheEyeTether.UnitTests
 {
     public class OSVersionHelpersTests
     {
+        [Fact]
+        public void GetProgramEnding_ThrowsNotImplementedException_WhenCalledOnLinux()
+        {
+            var osPlatformChecker = new OSPlatformCheckerStub(OSPlatform.Linux);
+
+            try
+            {
+                var result = OSVersionHelpers.GetProgramEnding(osPlatformChecker);
+            }
+            catch(System.Exception ex)
+            {
+                Assert.IsType<System.NotImplementedException>(ex);
+            }
+        }
     }
 }
--- TheEyeTether/Helpers/OSVersionHelpers.cs
+++ TheEyeTether/Helpers/OSVersionHelpers.cs
@@ -1,7 +1,12 @@
+using TheEyeTether.Interfaces;
 
 namespace TheEyeTether.Helpers
 {
     public static class OSVersionHelpers
     {
+        public static string GetProgramEnding(IOSPlatformChecker osPlatformChecker)
+        {
+            throw new System.NotImplementedException("No case has been created for current OSPlatform.");
+        }
     }    
 }
