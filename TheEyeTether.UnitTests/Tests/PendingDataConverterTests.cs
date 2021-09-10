5b436e592de40994aa7ada73749bc7353ace55ae
EdvardXYZ <eddumansky@gmail.com>
2021-09-10 10:59:22 +0300

    Create new file for each matching SavedVariables

--- TheEyeTether.UnitTests/Tests/PendingDataConverterTests.cs
+++ TheEyeTether.UnitTests/Tests/PendingDataConverterTests.cs
@@ -10,5 +10,23 @@ namespace TheEyeTether.UnitTests
 {
     public class PendingDataConverterTests
     {
+        [Fact]
+        public void Execute_CreatesFiles_WhenThereIsPendingData()
+        {
+            var programPath = @"C:\WorldOfWarcraft\_retail_\Wow.exe";
+            var pendingDataFilePath = @"C:\WorldOfWarcraft\_retail_\WTF\Account\AccountName\ServerName\CharacterName\SavedVariables\TheEyeRecorder.lua";
+            var fileSystemMock = new MockFileSystem(new Dictionary<string, MockFileData>()
+            {
+                { programPath, new MockFileData(string.Empty) },
+                { pendingDataFilePath, new MockFileData(string.Empty) }
+            });
+            var drivesGetterStub = new DrivesGetterStub(new List<string>() { @"C:\" });
+            var osPlatformCheckerStub = new OSPlatformCheckerStub(OSPlatform.Windows);
+
+            PendingDataConverter.Execute(fileSystemMock, drivesGetterStub, osPlatformCheckerStub);
+
+            var allFiles = fileSystemMock.AllFiles as string[];
+            Assert.Equal(3, allFiles.Length);
+        }
     }
 }
--- TheEyeTether/Types/PendingDataConverter.cs
+++ TheEyeTether/Types/PendingDataConverter.cs
@@ -5,5 +5,26 @@ namespace TheEyeTether.Types
 {
     public static class PendingDataConverter
     {
+        private const string FileName = "TheEyeRecorder.lua";
+        private const string ProgramName = "Wow";
+        private const string RequiredDirectories = @"WorldOfWarcraft\_retail_\";        
+
+
+        public static void Execute(
+                IFileSystem fileSystem,
+                IDrivesGetter drivesGetter,
+                IOSPlatformChecker osPlatformChecker)
+        {
+            var programPath = ProgramPathLocater.LocateProgramPath(ProgramName, RequiredDirectories,
+                    fileSystem, drivesGetter, osPlatformChecker);
+            var searchDirectoryPath = programPath.Replace(ProgramName + ".exe", string.Empty);
+            var filePaths = FilePathAggregator.AggregateFilePaths(FileName, searchDirectoryPath, fileSystem);
+
+            foreach(string filePath in filePaths)
+            {
+                var newFilePath = @"C:\test.txt";
+                fileSystem.File.Create(newFilePath);
+            }
+        }
     }
 }
