52e0293c2b821428999402c98a394a1a5e82e8c5
EdvardXYZ <eddumansky@gmail.com>
2021-09-11 16:48:30 +0300

    Return null when null snapshotTypes is passed

--- TheEyeTether.UnitTests/Tests/Types/SnapshotsCreatorTests.cs
+++ TheEyeTether.UnitTests/Tests/Types/SnapshotsCreatorTests.cs
@@ -7,11 +7,12 @@ namespace TheEyeTether.UnitTests.Tests.Types
     public class SnapshotsCreatorTests
     {
         [Fact]
-        public void Create_ReturnsListOfSnapshots_WhenPassedValidLuaTableData()
+        public void Create_ReturnsListOfSnapshots_WhenPassedValidLuaTableAndSnapshoTypes()
         {
             var luaTable = new Dictionary<object, object>();
+            var snapshotTypes = new string[1];
 
-            var result = SnapshotsCreator.Create(luaTable);
+            var result = SnapshotsCreator.Create(luaTable, snapshotTypes);
 
             Assert.IsType<List<Snapshot>>(result);
         }
@@ -20,8 +21,20 @@ namespace TheEyeTether.UnitTests.Tests.Types
         public void Create_ReturnsNull_WhenPassedNullLuaTable()
         {
             Dictionary<object, object> luaTable = null;
+            var snapshotTypes = new string[1];
 
-            var result = SnapshotsCreator.Create(luaTable);
+            var result = SnapshotsCreator.Create(luaTable, snapshotTypes);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Create_ReturnsNull_WhenPassedNullSnapshotTypes()
+        {
+            var luaTable = new Dictionary<object, object>();
+            string[] snapshotTypes = null;
+
+            var result = SnapshotsCreator.Create(luaTable, snapshotTypes);
 
             Assert.Null(result);
         }
--- TheEyeTether/Types/SnapshotsCreator.cs
+++ TheEyeTether/Types/SnapshotsCreator.cs
@@ -4,9 +4,11 @@ namespace TheEyeTether.Types
 {
     public static class SnapshotsCreator
     {
-        public static List<Snapshot> Create(Dictionary<object, object> luaTable)
+        public static List<Snapshot> Create(
+                Dictionary<object, object> luaTable,
+                string[] snapshotTypes)
         {
-            if(luaTable == null)
+            if(luaTable == null || snapshotTypes == null)
             {
                 return null;
             }
