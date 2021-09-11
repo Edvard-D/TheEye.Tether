9239f0d56e7adfa8568a68d2c6e615a3cd0578e1
EdvardXYZ <eddumansky@gmail.com>
2021-09-11 20:18:55 +0300

    Add data point type names field and property

--- TheEyeTether/Types/SnapshotType.cs
+++ TheEyeTether/Types/SnapshotType.cs
@@ -3,14 +3,19 @@ namespace TheEyeTether.Types
     public struct SnapshotType
     {
         private string _name;
+        private string[] _dataPointTypeNames;
 
 
         public string Name { get { return _name; } }
+        public string[] DataPointTypeNames { get { return _dataPointTypeNames; } }
 
 
-        public SnapshotType(string name)
+        public SnapshotType(
+                string name,
+                string[] dataPointTypeNames)
         {
             _name = name;
+            _dataPointTypeNames = dataPointTypeNames;
         }
     }
 }
