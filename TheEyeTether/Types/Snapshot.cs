41528c581a954d04ed10b25636cc93ecfaead50f
EdvardXYZ <eddumansky@gmail.com>
2021-09-11 20:16:21 +0300

    Create AddDataPoint function

--- TheEyeTether/Types/Snapshot.cs
+++ TheEyeTether/Types/Snapshot.cs
@@ -1,15 +1,25 @@
+using System.Collections.Generic;
+
 namespace TheEyeTether.Types
 {
     public struct Snapshot
     {
+        private List<DataPoint> _dataPoints;
         private string _tableName;
         private float _timestamp;
 
-
+        
+        public List<DataPoint> DataPoints { get { return _dataPoints; } }
         public Snapshot(string tableName, float timestamp)
         {
+            _dataPoints = new List<DataPoint>();
             _tableName = tableName;
             _timestamp = timestamp;
+        }
+
+        public void AddDataPoint(DataPoint dataPoint)
+        {
+            _dataPoints.Add(dataPoint);
         }
     }
 }
