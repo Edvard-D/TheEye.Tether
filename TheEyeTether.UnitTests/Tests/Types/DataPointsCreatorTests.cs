b0a5ea6034a4eb451c70b46215bf25e05d185fb6
EdvardXYZ <eddumansky@gmail.com>
2021-09-12 17:18:03 +0300

    Return dictionary of lists of DataPoints

--- TheEyeTether.UnitTests/Tests/Types/DataPointsCreatorTests.cs
+++ TheEyeTether.UnitTests/Tests/Types/DataPointsCreatorTests.cs
@@ -1,6 +1,22 @@
+using System.Collections.Generic;
+using TheEyeTether.Types;
+using Xunit;
+
 namespace TheEyeTether.UnitTests.Tests.Types
 {
-    public class DataPointsCreator
+    public class DataPointsCreatorTests
     {
+        [Fact]
+        public void Create_ReturnsDictionaryOfListsOfDataPoints_WhenPassedValidLuaTable()
+        {
+            var luaTable = new Dictionary<object, object>()
+            {
+                { "test", new Dictionary<object, object>() { { 1, 1f } } }
+            };
+
+            var result = DataPointsCreator.Create(luaTable);
+
+            Assert.IsType<Dictionary<string, List<DataPoint>>>(result);
+        }
     }
 }
--- TheEyeTether/Types/DataPointsCreator.cs
+++ TheEyeTether/Types/DataPointsCreator.cs
@@ -1,6 +1,14 @@
+using System.Collections.Generic;
+
 namespace TheEyeTether.Types
 {
     public static class DataPointsCreator
     {
+        public static Dictionary<string, List<DataPoint>> Create(Dictionary<object, object> luaTable)
+        {
+            var dataPoints = new Dictionary<string, List<DataPoint>>();
+
+            return dataPoints;
+        }
     }
 }
