bf09bfac3967a172286fd41229f44ab768534a0c
EdvardXYZ <eddumansky@gmail.com>
2021-09-13 16:01:26 +0300

    Refactor TimestampRange to be a record

--- TheEyeTether/Types/TimestampRange.cs
+++ TheEyeTether/Types/TimestampRange.cs
@@ -1,50 +1,11 @@
 namespace TheEyeTether.Types
 {
-    public struct TimestampRange
+    public record TimestampRange
     {
-        private float _end;
-        private float _start;
-
-        
-        public float End { get { return _end; } }
-        public float Start { get { return _start; } }
-
-        
-        public TimestampRange(float start, float end)
-        {
-            _end = end;
-            _start = start;
-        }
+        public float End;
+        public float Start;
 
 
-        public override bool Equals(object obj)
-        {            
-            return obj is TimestampRange && Equals((TimestampRange)obj);
-        }
-        public bool Equals(TimestampRange timestampRange)
-        {
-            return timestampRange._end == _end
-                    && timestampRange._start == _start;
-        }
-
-        public override int GetHashCode()
-        {
-            return System.HashCode.Combine(_end, _start);
-        }
-
-        public static bool operator ==(TimestampRange timestampRange1, TimestampRange timestampRange2)
-        {
-            return timestampRange1.Equals(timestampRange2);
-        }
-
-        public static bool operator !=(TimestampRange timestampRange1, TimestampRange timestampRange2)
-        {
-            return !timestampRange1.Equals(timestampRange2);
-        }
-
-        public bool IsTimestampInRange(float timestamp)
-        {
-            return timestamp >= _start && timestamp < _end;
-        }
+        public TimestampRange(float start, float end) => (Start, End) = (start, end);
     }
 }
