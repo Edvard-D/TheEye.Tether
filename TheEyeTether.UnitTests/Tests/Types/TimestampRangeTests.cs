da32c1a2f031d55465cc262e030d1c08ff96b50b
EdvardXYZ <eddumansky@gmail.com>
2021-09-13 15:38:51 +0300

    Fix and add equality tests

--- TheEyeTether.UnitTests/Tests/Types/TimestampRangeTests.cs
+++ TheEyeTether.UnitTests/Tests/Types/TimestampRangeTests.cs
@@ -20,6 +20,28 @@ namespace TheEyeTether.UnitTests.Tests.Types
         public void Equals_ReturnsFalse_WhenAllValuesAreNotEqual()
         {
             var value1 = new TimestampRange(0, 0);
+            var value2 = new TimestampRange(1, 1);
+
+            var result = value1.Equals(value2);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Equals_ReturnsFalse_WhenStartValueIsNotEqual()
+        {
+            var value1 = new TimestampRange(0, 0);
+            var value2 = new TimestampRange(1, 0);
+
+            var result = value1.Equals(value2);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Equals_ReturnsFalse_WhenEndValueIsNotEqual()
+        {
+            var value1 = new TimestampRange(0, 0);
             var value2 = new TimestampRange(0, 1);
 
             var result = value1.Equals(value2);
