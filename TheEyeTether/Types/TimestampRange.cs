42098f1018e259ab3b7682084d9a5c2dc259bba1
EdvardXYZ <eddumansky@gmail.com>
2021-09-13 15:32:32 +0300

    Override Equals method

--- TheEyeTether/Types/TimestampRange.cs
+++ TheEyeTether/Types/TimestampRange.cs
@@ -15,5 +15,36 @@ namespace TheEyeTether.Types
             _end = end;
             _start = start;
         }
+
+
+        public override bool Equals(object obj)
+        {            
+            return obj is TimestampRange && Equals((TimestampRange)obj);
+        }
+        public bool Equals(TimestampRange timestampRange)
+        {
+            return timestampRange._end == _end
+                    && timestampRange._start == _start;
+        }
+
+        public override int GetHashCode()
+        {
+            return System.HashCode.Combine(_end, _start);
+        }
+
+        public static bool operator ==(TimestampRange timestampRange1, TimestampRange timestampRange2)
+        {
+            return timestampRange1.Equals(timestampRange2);
+        }
+
+        public static bool operator !=(TimestampRange timestampRange1, TimestampRange timestampRange2)
+        {
+            return !timestampRange1.Equals(timestampRange2);
+        }
+
+        public bool IsTimestampInRange(float timestamp)
+        {
+            return timestamp >= _start && timestamp < _end;
+        }
     }
 }
