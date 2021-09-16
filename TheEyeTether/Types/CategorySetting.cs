fddad28dfc79c581c2e71362b22922b4b8abe32b
EdvardXYZ <eddumansky@gmail.com>
2021-09-16 13:40:11 +0300

    Create CategorySetting record

---
+++ TheEyeTether/Types/CategorySetting.cs
@@ -0,0 +1,10 @@
+namespace TheEyeTether.Types
+{
+    public record CategorySetting
+    {
+        public string Name;
+
+
+        public CategorySetting(string name) => (Name) = (name);
+    }
+}
