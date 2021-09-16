15410ffa82ee3044799c244bdbc8ec0636c45433
EdvardXYZ <eddumansky@gmail.com>
2021-09-16 13:41:06 +0300

    Return dictionary value

--- TheEyeTether.UnitTests/Tests/Types/CategoriesCreatorTests.cs
+++ TheEyeTether.UnitTests/Tests/Types/CategoriesCreatorTests.cs
@@ -1,6 +1,26 @@
+using System.Collections.Generic;
+using TheEyeTether.Types;
+using Xunit;
+
 namespace TheEyeTether.UnitTests.Tests.Types
 {
     public class CategoriesCreatorTests
     {
+        [Fact]
+        public void Create_ReturnsADictionaryOfCategorySettingCategoryPairs_WhenPassedValidLuaTable()
+        {
+            var categoryName = "test1";
+            var subTableName = "test2";
+            var subTable = new Dictionary<object, object>()
+            {
+                { subTableName, new Dictionary<object, object>() { { 1, 1f } } }
+            };
+            var luaTable = new Dictionary<object, object>() { { categoryName, subTable } };
+            var categorySettings = new CategorySetting[] { new CategorySetting(categoryName) };
+
+            var result = CategoriesCreator.Create(luaTable, categorySettings);
+
+            Assert.IsType<Dictionary<CategorySetting, Category>>(result);
+        }
     }
 }
--- TheEyeTether/Types/CategoriesCreator.cs
+++ TheEyeTether/Types/CategoriesCreator.cs
@@ -1,6 +1,14 @@
+using System.Collections.Generic;
+
 namespace TheEyeTether.Types
 {
     public static class CategoriesCreator
     {
+        public static Dictionary<CategorySetting, Category> Create(
+                Dictionary<object, object> luaTable,
+                CategorySetting[] categorySettings)
+        {
+            return new Dictionary<CategorySetting, Category>();
+        }
     }
 }
