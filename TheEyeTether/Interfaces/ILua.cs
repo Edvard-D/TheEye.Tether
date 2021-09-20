7dc5aee2cbfc79271fe3cc95293a83220eb5a7df
EdvardXYZ <eddumansky@gmail.com>
2021-09-20 16:48:38 +0300

    Add ConvertTableHierarchyToDict default method

--- TheEyeTether/Interfaces/ILua.cs
+++ TheEyeTether/Interfaces/ILua.cs
@@ -5,6 +5,28 @@ namespace TheEyeTether.Interfaces
 {
     public interface ILua
     {
+        Dictionary<object, object> ConvertTableHierarchyToDict(LuaTable luaTable)
+        {
+            var outputDict = new Dictionary<object, object>();
+
+            foreach(KeyValuePair<object, object> keyValuePair in luaTable)
+            {
+                var valueTable = keyValuePair.Value as LuaTable;
+                var valuesEnumerator = valueTable.Values.GetEnumerator();
+                valuesEnumerator.MoveNext();
+
+                if(valuesEnumerator.Current.GetType() == typeof(LuaTable))
+                {
+                    outputDict[keyValuePair.Key] = ConvertTableHierarchyToDict(valueTable);
+                }
+                else
+                {
+                    outputDict[keyValuePair.Key] = GetTableDict(valueTable);
+                }
+            }
+
+            return outputDict;
+        }
         bool DoesFileExist(string filePath);
         object[] DoFile(string filePath);
         LuaTable GetTable(string tableName);
