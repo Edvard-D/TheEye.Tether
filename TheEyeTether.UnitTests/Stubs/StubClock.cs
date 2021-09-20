19faba59c9ae4868b4338e9b0bc2ae78e5105af0
EdvardXYZ <eddumansky@gmail.com>
2021-09-20 18:08:00 +0300

    Create StubClock class

---
+++ TheEyeTether.UnitTests/Stubs/StubClock.cs
@@ -0,0 +1,19 @@
+using System;
+using TheEyeTether.Interfaces;
+
+namespace TheEyeTether.UnitTests.Stubs
+{
+    public class StubClock : IClock
+    {
+        private DateTime _now;
+
+        
+        public StubClock(DateTime now)
+        {
+            _now = now;
+        }
+
+
+        public DateTime Now { get { return _now; } }
+    }
+}
