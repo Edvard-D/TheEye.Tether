427f9d34069eb3cf6746e20b07935c329a627f64
EdvardXYZ <eddumansky@gmail.com>
2021-09-21 15:03:46 +0300

    Refactor Snapshot into a record

--- TheEyeTether/Types/Snapshot.cs
+++ TheEyeTether/Types/Snapshot.cs
@@ -2,45 +2,16 @@ using System.Collections.Generic;
 
 namespace TheEyeTether.Types
 {
-    public struct Snapshot
+    public record Snapshot
     {
-        private DataPoint _dataPoint;
-        private List<string> _dataPoints;
-
-        
-        public DataPoint DataPoint { get { return _dataPoint; } }
-        public List<string> DataPointsIds { get { return _dataPoints; } }
+        public DataPoint DataPoint;
+        public List<string> DataPointsIds;
 
 
         public Snapshot(DataPoint dataPoint)
         {
-            _dataPoint = dataPoint;
-            _dataPoints = new List<string>();
-        }
-
-        public override bool Equals(object obj)
-        {            
-            return obj is Snapshot && Equals((Snapshot)obj);
-        }
-        public bool Equals(Snapshot snapshot)
-        {
-            return snapshot._dataPoint == _dataPoint
-                    && snapshot._dataPoints == _dataPoints;
-        }
-
-        public override int GetHashCode()
-        {
-            return System.HashCode.Combine(_dataPoint, _dataPoints);
-        }
-
-        public static bool operator ==(Snapshot snapshot1, Snapshot snapshot2)
-        {
-            return snapshot1.Equals(snapshot2);
-        }
-
-        public static bool operator !=(Snapshot snapshot1, Snapshot snapshot2)
-        {
-            return !snapshot1.Equals(snapshot2);
+            DataPoint = dataPoint;
+            DataPointsIds = new List<string>();
         }
     }
 }
