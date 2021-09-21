3f96166946acf5f48c9768561067c9779d03c3ee
EdvardXYZ <eddumansky@gmail.com>
2021-09-21 16:17:49 +0300

    Return list of hypotheses

--- TheEyeTether.UnitTests/Tests/Types/HypothesesCreatorTests.cs
+++ TheEyeTether.UnitTests/Tests/Types/HypothesesCreatorTests.cs
@@ -1,6 +1,17 @@
+using System.Collections.Generic;
+using TheEyeTether.Types;
+using Xunit;
+
 namespace TheEyeTether.UnitTests.Tests.Types
 {
     public class HypothesesCreatorTests
     {
+        [Fact]
+        public void Create_ReturnsListOfHypotheses_WhenCalled()
+        {
+            var result = HypothesesCreator.Create();
+
+            Assert.IsType<List<Hypothesis>>(result);
+        }
     }
 }
--- TheEyeTether/Types/HypothesesCreator.cs
+++ TheEyeTether/Types/HypothesesCreator.cs
@@ -1,6 +1,12 @@
+using System.Collections.Generic;
+
 namespace TheEyeTether.Types
 {
     public static class HypothesesCreator
     {
+        public static List<Hypothesis> Create()
+        {
+            return new List<Hypothesis>();
+        }
     }
 }
