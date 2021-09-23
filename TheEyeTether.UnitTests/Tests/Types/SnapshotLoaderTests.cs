f1f5292e41d8c36c2494f877161dd4dc9cfd64ed
EdvardXYZ <eddumansky@gmail.com>
2021-09-23 16:40:10 +0300

    Return list of snapshots

--- TheEyeTether.UnitTests/Tests/Types/SnapshotLoaderTests.cs
+++ TheEyeTether.UnitTests/Tests/Types/SnapshotLoaderTests.cs
@@ -1,6 +1,17 @@
+using System.Collections.Generic;
+using TheEyeTether.Types;
+using Xunit;
+
 namespace TheEyeTether.UnitTests.Tests.Types
 {
     public class SnapshotLoaderTests
     {
+        [Fact]
+        public void Load_ReturnsListOfSnapshots_WhenCalled()
+        {
+            var result = SnapshotLoader.Load();
+
+            Assert.IsType<List<Snapshot>>(result);
+        }
     }
 }
--- TheEyeTether/Types/SnapshotLoader.cs
+++ TheEyeTether/Types/SnapshotLoader.cs
@@ -1,6 +1,12 @@
+using System.Collections.Generic;
+
 namespace TheEyeTether.Types
 {
     public static class SnapshotLoader
     {
+        public static List<Snapshot> Load()
+        {
+            return new List<Snapshot>();
+        }
     }
 }
