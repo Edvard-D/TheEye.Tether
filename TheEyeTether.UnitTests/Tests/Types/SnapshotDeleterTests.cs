4723cc6e8c028bae107311535d1fbf9a6d801476
EdvardXYZ <eddumansky@gmail.com>
2021-09-27 15:09:06 +0300

    Throw exception when passed null value

--- TheEyeTether.UnitTests/Tests/Types/SnapshotDeleterTests.cs
+++ TheEyeTether.UnitTests/Tests/Types/SnapshotDeleterTests.cs
@@ -1,6 +1,23 @@
+using System.IO.Abstractions.TestingHelpers;
+using TheEyeTether.Types;
+using Xunit;
+
 namespace TheEyeTether.UnitTests.Tests.Types
 {
     public class SnapshotDeleterTests
     {
+        [Fact]
+        public void DeleteOutdatedFiles_ThrowsInvalidOperationException_WhenPassedNullDirectoryPath()
+        {
+            try
+            {
+                SnapshotDeleter.DeleteOutdatedFiles(null);
+                Assert.True(false);
+            }
+            catch(System.Exception ex)
+            {
+                Assert.IsType<System.InvalidOperationException>(ex);
+            }
+        }
     }
 }
--- TheEyeTether/Types/SnapshotDeleter.cs
+++ TheEyeTether/Types/SnapshotDeleter.cs
@@ -2,5 +2,14 @@ namespace TheEyeTether.Types
 {
     public static class SnapshotDeleter
     {
+        public static void DeleteOutdatedFiles(
+                string directoryPath)
+        {
+            if(directoryPath == null)
+            {
+                throw new System.InvalidOperationException(string.Format("Argument {0} cannot be null.",
+                        nameof(directoryPath)));
+            }
+        }
     }
 }
