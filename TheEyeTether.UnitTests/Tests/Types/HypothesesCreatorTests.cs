d3b66ee5e76418f41ddea294ea793a7e84c0f40d
EdvardXYZ <eddumansky@gmail.com>
2021-09-27 16:16:24 +0300

    Delete outdated files

--- TheEyeTether.UnitTests/Tests/Types/HypothesesCreatorTests.cs
+++ TheEyeTether.UnitTests/Tests/Types/HypothesesCreatorTests.cs
@@ -1,17 +1,49 @@
 using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
 using TheEyeTether.Types;
+using TheEyeTether.UnitTests.Stubs;
 using Xunit;
 
 namespace TheEyeTether.UnitTests.Tests.Types
 {
     public class HypothesesCreatorTests
     {
+        private const string DateTimeFormat = "yyyy_MM_dd__HH_mm_ss";
+        private const string NowDateTimeString = "2021_09_23__12_00_00";
+
+
         [Fact]
         public void Create_ReturnsListOfHypotheses_WhenCalled()
         {
-            var result = HypothesesCreator.Create();
+            var nowDateTime = System.DateTime.ParseExact(NowDateTimeString, DateTimeFormat, null)
+                    .ToUniversalTime();
+            var mockFileSystem = new MockFileSystem();
+            var stubClock = new StubClock(nowDateTime);
+
+            var result = HypothesesCreator.Create(mockFileSystem, stubClock);
 
             Assert.IsType<List<Hypothesis>>(result);
+        }
+
+        [Fact]
+        public void Create_DeletesFile_WhenOlderThan7Days()
+        {
+            var nowDateTime = System.DateTime.ParseExact(NowDateTimeString, DateTimeFormat, null)
+                    .ToUniversalTime();
+            var creationDateTime = nowDateTime.AddDays(-8);
+            var directoryPath = @"C:\";
+            var fileName = creationDateTime.ToString(DateTimeFormat) + ".json";
+            var mockFileData = new MockFileData(string.Empty);
+            mockFileData.CreationTime = creationDateTime;
+            var mockFileSystem = new MockFileSystem(new Dictionary<string, MockFileData>()
+            {
+                { directoryPath + fileName, mockFileData }
+            });
+            var stubClock = new StubClock(nowDateTime);
+
+            var result = HypothesesCreator.Create(mockFileSystem, stubClock);
+
+            Assert.DoesNotContain(directoryPath + fileName, mockFileSystem.AllFiles);
         }
     }
 }
--- TheEyeTether/Types/HypothesesCreator.cs
+++ TheEyeTether/Types/HypothesesCreator.cs
@@ -1,11 +1,20 @@
 using System.Collections.Generic;
+using System.IO.Abstractions;
+using TheEyeTether.Interfaces;
 
 namespace TheEyeTether.Types
 {
     public static class HypothesesCreator
     {
-        public static List<Hypothesis> Create()
+        private const int SnapshotKeepLookbackDays = 7;
+
+
+        public static List<Hypothesis> Create(
+                IFileSystem fileSystem,
+                IClock clock)
         {
+            SnapshotDeleter.DeleteOutdatedFiles(@"C:\", SnapshotKeepLookbackDays, fileSystem, clock);
+
             return new List<Hypothesis>();
         }
     }
