688946265088a8ce7f9cbb0fdb04d48e52ed00c6
EdvardXYZ <eddumansky@gmail.com>
2021-10-01 15:11:50 +0300

    Add hypothesis to list

--- TheEyeTether.UnitTests/Tests/Extensions/ListHypothesisExtensionsTests.cs
+++ TheEyeTether.UnitTests/Tests/Extensions/ListHypothesisExtensionsTests.cs
@@ -1,6 +1,22 @@
+using System.Collections.Generic;
+using TheEyeTether.Extensions;
+using TheEyeTether.Types;
+using Xunit;
+
 namespace TheEyeTether.UnitTests.Tests.Extensions
 {
     public class ListHypothesisExtensionsTests
     {
+        [Fact]
+        public void AddUnique_AddsHypothesisToList_WhenListDoesNotHaveAHypothesisWithMatchingDataPointStrings()
+        {
+            var dataPointStrings1 = new HashSet<string>() { "test1" };
+            var hypotheses = new List<Hypothesis>() { new Hypothesis(dataPointStrings1) };
+
+            var dataPointStrings2 = new HashSet<string>() { "test2" };
+            hypotheses.AddUnique(new Hypothesis(dataPointStrings2));
+
+            Assert.Equal(2, hypotheses.Count);
+        }
     }
 }
--- TheEyeTether/Extensions/ListHypothesisExtensions.cs
+++ TheEyeTether/Extensions/ListHypothesisExtensions.cs
@@ -1,6 +1,15 @@
+using System.Collections.Generic;
+using TheEyeTether.Types;
+
 namespace TheEyeTether.Extensions
 {
     public static class ListHypothesisExtensions
     {
+        public static void AddUnique(
+                this List<Hypothesis> list,
+                Hypothesis hypothesis)
+        {
+            list.Add(hypothesis);
+        }
     }
 }
