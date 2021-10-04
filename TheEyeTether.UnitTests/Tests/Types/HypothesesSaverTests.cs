acee837484d5962073d81cecdb61019488c02a6a
EdvardXYZ <eddumansky@gmail.com>
2021-10-04 18:17:14 +0300

    Create file when it does not exist

--- TheEyeTether.UnitTests/Tests/Types/HypothesesSaverTests.cs
+++ TheEyeTether.UnitTests/Tests/Types/HypothesesSaverTests.cs
@@ -1,6 +1,28 @@
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using TheEyeTether.Types;
+using TheEyeTether.UnitTests.Stubs;
+using Xunit;
+
 namespace TheEyeTether.UnitTests.Tests.Types
 {
     public class HypothesesSaverTests
     {
+        private const string ProgramPath = @"C:\TestProgram\";
+        private const string SavePath = ProgramPath + @"Data\Hypotheses.json";
+
+        
+        [Fact]
+        public void Save_CreatesFile_WhenItDoesNotExistAndThereIsAHypothesesToSave()
+        {
+            var dataPointStrings = new HashSet<string>() { "testDataPointString" };
+            var hypotheses = new List<Hypothesis>() { new Hypothesis(dataPointStrings) };
+            var mockFileSystem = new MockFileSystem();
+            var stubCurrentDomainBaseDirectoryGetter = new StubCurrentDomainBaseDirectoryGetter(ProgramPath);
+
+            HypothesesSaver.Save(hypotheses, mockFileSystem, stubCurrentDomainBaseDirectoryGetter);
+
+            Assert.Contains(SavePath, mockFileSystem.AllFiles);
+        }
     }
 }
--- TheEyeTether/Types/HypothesesSaver.cs
+++ TheEyeTether/Types/HypothesesSaver.cs
@@ -1,6 +1,25 @@
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using TheEyeTether.Interfaces;
+
 namespace TheEyeTether.Types
 {
     public static class HypothesesSaver
     {
+        private const string SaveFileName = "Hypotheses.json";
+
+
+        public static void Save(
+                List<Hypothesis> hypotheses,
+                IFileSystem fileSystem,
+                ICurrentDomainBaseDirectoryGetter currentDomainBaseDirectoryGetter)
+        {
+            var currentDomainBaseDirectory = currentDomainBaseDirectoryGetter.GetCurrentDomainBaseDirectory();
+            var outputFilePathDirectory = fileSystem.Path.Combine(currentDomainBaseDirectory, "Data");
+            fileSystem.Directory.CreateDirectory(outputFilePathDirectory);
+            var outputFilePath = fileSystem.Path.Combine(outputFilePathDirectory, SaveFileName);
+
+            fileSystem.File.WriteAllText(outputFilePath, string.Empty);
+        }
     }
 }
