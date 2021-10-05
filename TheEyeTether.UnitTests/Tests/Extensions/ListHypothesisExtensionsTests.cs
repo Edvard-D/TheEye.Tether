87ace3bf51deb5257fbb61401b88b4d79b475485
EdvardXYZ <eddumansky@gmail.com>
2021-10-05 14:59:29 +0300

    Refactor tests

--- TheEyeTether.UnitTests/Tests/Extensions/ListHypothesisExtensionsTests.cs
+++ TheEyeTether.UnitTests/Tests/Extensions/ListHypothesisExtensionsTests.cs
@@ -7,12 +7,20 @@ namespace TheEyeTether.UnitTests.Tests.Extensions
 {
     public class ListHypothesisExtensionsTests
     {
+        private const string CategoryId = "testCategoryId";
+        private const string CategoryType = "testCategoryType";
+        private const string DataPointString = "testDataPointString";
+        private const string SnapshotId = "testSnapshotId";
+        private const string SnapshotType = "testSnapshotType";
+
+
         [Fact]
-        public void AddUnique_AddsHypothesisToList_WhenListDoesNotHaveAHypothesisWithMatchingDataPointStrings()
+        public void AddUnique_AddsHypothesisToList_WhenAllValueMatchExceptDataPointStrings()
         {
-            var dataPointStrings1 = new HashSet<string>() { "test1" };
-            var dataPointStrings2 = new HashSet<string>() { "test2" };
-            var hypotheses = new List<Hypothesis>() { new Hypothesis(dataPointStrings1) };
+            var dataPointStrings1 = new HashSet<string>() { DataPointString + "1" };
+            var dataPointStrings2 = new HashSet<string>() { DataPointString + "2" };
+            var hypotheses = new List<Hypothesis>() { new Hypothesis(CategoryType, CategoryId,
+                    SnapshotType, SnapshotId, dataPointStrings1) };
 
             hypotheses.AddUnique(new Hypothesis(dataPointStrings2));
 
@@ -20,27 +28,29 @@ namespace TheEyeTether.UnitTests.Tests.Extensions
         }
 
         [Fact]
-        public void AddUnique_DoesNotAddHypothesisToList_WhenListDoesHaveAHypothesisWithMatchingDataPointStrings()
+        public void AddUnique_DoesNotAddHypothesisToList_WhenAllValuesMatch()
         {
-            var dataPointStrings1 = new HashSet<string>() { "test1" };
-            var hypotheses = new List<Hypothesis>() { new Hypothesis(dataPointStrings1) };
+            var dataPointStrings = new HashSet<string>() { DataPointString };
+            var hypotheses = new List<Hypothesis>() { new Hypothesis(CategoryType, CategoryId,
+                    SnapshotType, SnapshotId, dataPointStrings) };
 
-            hypotheses.AddUnique(new Hypothesis(dataPointStrings1));
+            hypotheses.AddUnique(new Hypothesis(dataPointStrings));
 
             Assert.Single(hypotheses);
         }
 
         [Fact]
-        public void AddUniques_AddsHypothesesToList_WhenListDoesNotHaveHypothesesWithMatchingDataPointStrings()
+        public void AddUniques_AddsHypothesesToList_WhenElementWithAllValuesMatchingExceptDataPointStringExists()
         {
-            var dataPointStrings1 = new HashSet<string>() { "test1" };
-            var dataPointStrings2 = new HashSet<string>() { "test2" };
-            var dataPointStrings3 = new HashSet<string>() { "test3" };
-            var hypotheses = new List<Hypothesis>() { new Hypothesis(dataPointStrings1) };
+            var dataPointStrings1 = new HashSet<string>() { DataPointString + "1" };
+            var dataPointStrings2 = new HashSet<string>() { DataPointString + "2" };
+            var dataPointStrings3 = new HashSet<string>() { DataPointString + "3" };
+            var hypotheses = new List<Hypothesis>() { new Hypothesis(CategoryType, CategoryId,
+                    SnapshotType, SnapshotId, dataPointStrings1) };
             var newHypotheses = new List<Hypothesis>()
             {
-                new Hypothesis(dataPointStrings2),
-                new Hypothesis(dataPointStrings3)
+                new Hypothesis(CategoryType, CategoryId, SnapshotType, SnapshotId, dataPointStrings2),
+                new Hypothesis(CategoryType, CategoryId, SnapshotType, SnapshotId, dataPointStrings3),
             };
 
             hypotheses.AddUniques(newHypotheses);
@@ -49,14 +59,14 @@ namespace TheEyeTether.UnitTests.Tests.Extensions
         }
 
         [Fact]
-        public void AddUniques_DoesNotAddHypothesesToList_WhenListHasHypothesesWithMatchingDataPointStrings()
+        public void AddUniques_DoesNotAddHypothesesToList_WhenElementWithAllValuesMatchingExists()
         {
-            var dataPointStrings1 = new HashSet<string>() { "test1" };
-            var dataPointStrings2 = new HashSet<string>() { "test2" };
+            var dataPointStrings1 = new HashSet<string>() { DataPointString + "1" };
+            var dataPointStrings2 = new HashSet<string>() { DataPointString + "2" };
             var hypotheses = new List<Hypothesis>()
             {
-                new Hypothesis(dataPointStrings1),
-                new Hypothesis(dataPointStrings2)
+                new Hypothesis(CategoryType, CategoryId, SnapshotType, SnapshotId, dataPointStrings1),
+                new Hypothesis(CategoryType, CategoryId, SnapshotType, SnapshotId, dataPointStrings2),
             };
 
             hypotheses.AddUniques(hypotheses);
